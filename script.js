d34ba944b6f4444fd317daa57bde286cff846850
tarun_upadhyay <2020kucp1019@iiitkota.ac.in>
2021-07-13 15:18:50 +0530

    Project Completed

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-
-// const daysE1= document.getElementById('days');
-const hoursEl= document.getElementById('hours');
-const minsE1= document.getElementById('mins');
-const secondsE1= document.getElementById('seconds');
-
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
 const newYears = "1 Jan 2022";
 
-function countdown(){
+function countdown() {
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
-    
+
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
-    
-
     const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 60;
-
+    const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    hoursEl.innerHTML = hours;
-    minsE1.innerHTML = mins;
-    secondsE1.innerHTML = seconds;
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
 
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
 
-//Initial call
+// initial call
 countdown();
 
 setInterval(countdown, 1000);
